e760f43b81c1e921cbcf51fb668eed58f14e4557
何子杰 <1710276127@qq.com>
2020-04-14 14:10:30 +0800

    自定义viewgroup中margin和padding的处理

--- app/src/main/java/com/zijie/frameworksource_4/TestViewGroup.java
+++ app/src/main/java/com/zijie/frameworksource_4/TestViewGroup.java
@@ -34,11 +34,17 @@ public class TestViewGroup extends ViewGroup {
         int height = MeasureSpec.getSize(heightMeasureSpec);
         int measureWidth = 0;
         int measureHeight = 0;
+        int marginL = 0;
+        int marginR = 0;
+        int marginTop = 0;
+        int marginB = 0;
         //根据父控件的测量参数，测量子view
         int count = getChildCount();
         for (int i = 0; i < count; i++) {
             View childAt = getChildAt(i);
-            LayoutParams layoutParams = childAt.getLayoutParams();
+            MarginLayoutParams layoutParams = (MarginLayoutParams) childAt.getLayoutParams();
+            marginTop += layoutParams.topMargin;
+            marginB += layoutParams.bottomMargin;
             int childMeasureSpec = getChildMeasureSpec(widthMeasureSpec, childAt.getPaddingLeft() + childAt.getPaddingRight() , layoutParams.width);
             int childMeasureSpec1 = getChildMeasureSpec(heightMeasureSpec, childAt.getPaddingTop() + childAt.getPaddingBottom(), layoutParams.height);
             childAt.measure(childMeasureSpec,childMeasureSpec1);
@@ -52,7 +58,10 @@ public class TestViewGroup extends ViewGroup {
             case MeasureSpec.UNSPECIFIED://
                 for (int i = 0; i < count; i++) {
                     View childAt = getChildAt(i);
-                    measureWidth = Math.max(measureWidth,i * offSet + childAt.getMeasuredWidth());
+                    MarginLayoutParams layoutParams = (MarginLayoutParams) childAt.getLayoutParams();
+                    marginL = Math.max(0,layoutParams.leftMargin);//计算子view的最大
+                    marginR = Math.max(0,layoutParams.rightMargin);
+                    measureWidth = Math.max(measureWidth,i * offSet + childAt.getMeasuredWidth() + marginL + marginR);
                 }
                 break;
             default:
@@ -73,19 +82,27 @@ public class TestViewGroup extends ViewGroup {
                 Log.e(TAG, "onMeasure: 都没有执行"  );
                 break;
         }
+        measureWidth += getPaddingLeft() + getPaddingRight();
+        measureHeight += getPaddingTop() + getPaddingBottom() + marginTop + marginB;
         Log.e(TAG, "onMeasure: " + measureWidth + "----" + measureHeight );
         setMeasuredDimension(measureWidth,measureHeight);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        int top = 0,bottom = 0,left = 0,right = 0;
+        int top = getPaddingTop(),bottom = 0,left = 0,right = 0;
         for (int i = 0; i < getChildCount(); i++) {
+            View childAt = getChildAt(i);
+            MarginLayoutParams layoutParams = (MarginLayoutParams) childAt.getLayoutParams();
             if (i > 0){
-                top += getChildAt(i - 1).getMeasuredHeight();
+                View lastView = getChildAt(i - 1);
+                MarginLayoutParams lastViewParams = (MarginLayoutParams) lastView.getLayoutParams();
+                top += getChildAt(i - 1).getMeasuredHeight() + layoutParams.topMargin + lastViewParams.bottomMargin;
+            }else {
+                top += layoutParams.topMargin;
             }
             bottom = top + getChildAt(i).getMeasuredHeight();
-            left = i * offSet;
+            left = layoutParams.leftMargin + getPaddingLeft() + i * offSet;
             right = left + getChildAt(i).getMeasuredWidth();
             Log.e(TAG, "onLayout: " + left + "----" + top+ "----" + right + "----" + bottom  );
             getChildAt(i).layout(left,top,right,bottom);
